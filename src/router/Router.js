44156ef1708d23709dc8de3d7b444c5910979920
BrunoBeltreGuzman <brunodevv@gmail.com>
2021-01-27 18:37:55 -0400

    dev-folio v1 gh-pages added

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,34 +1,16 @@
 import React from "react";
-import {
-       BrowserRouter as Router,
-       Route,
-       Switch,
-       HashRouter,
-       Link,
-} from "react-router-dom";
 import DevFolio from "../pages/DevFolio";
-import NotFound from "../pages/NotFound";
+
 export default function router() {
-       return (
+       return <DevFolio></DevFolio>;
+}
+
+//<Route component={NotFound} />
+/*
               <HashRouter basename="/">
                      <Route exact path="/">
                             <DevFolio></DevFolio>
                      </Route>
                      <Route component={NotFound} />
               </HashRouter>
-       );
-
-       return (
-              <div>
-                     <Router>
-                            <Switch>
-                                   <Route exact path="/">
-                                          <DevFolio></DevFolio>
-                                   </Route>
-                                   <Route component={NotFound} />
-                            </Switch>
-                     </Router>
-              </div>
-       );
-}
-//<Route component={NotFound} />
+*/
